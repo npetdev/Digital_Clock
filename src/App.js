b3cc0016f9bd909652e3e953aebc5da8c00798f8
npetdev <nebojsa.p18@gmail.com>
2024-07-16 15:42:25 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       React Clock
+      </header>
+    </div>
+  );
+}
+
+export default App;
